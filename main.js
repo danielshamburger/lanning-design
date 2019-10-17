47c12e6961fcabe9046b02782e9a1a9d8190758b
Daniel Shamburger <daniel@wearetbx.com>
2019-10-17 15:49:54 -0400

    cleanup

--- main.js
+++ main.js
@@ -6,25 +6,25 @@ const closeButtons = document.getElementsByClassName("close-window");
 const openLinks = document.getElementsByClassName("open-link");
 
 Array.from(closeButtons).forEach(function(element) {
-	element.addEventListener('click', function(event) {
-		document.getElementById(this.dataset.close).style.display = 'none';
-	});
+  element.addEventListener("click", function(event) {
+    document.getElementById(this.dataset.close).style.display = "none";
   });
+});
 
-  Array.from(openLinks).forEach(function(element) {
-	element.addEventListener('click', function(event) {
-		event.preventDefault();
-		for (let item of guiWindows) {
-			// console.log(item);
-			if ( item.classList.contains("active-window") ) {
-				item.classList.remove("active-window");
-			}
-		  }
-		let activeWindow = document.getElementById(this.dataset.open);
-		activeWindow.style.display = 'block';
-		activeWindow.classList.add('active-window');
-	});
+Array.from(openLinks).forEach(function(element) {
+  element.addEventListener("click", function(event) {
+    event.preventDefault();
+    for (let item of guiWindows) {
+      // console.log(item);
+      if (item.classList.contains("active-window")) {
+        item.classList.remove("active-window");
+      }
+    }
+    let activeWindow = document.getElementById(this.dataset.open);
+    activeWindow.style.display = "block";
+    activeWindow.classList.add("active-window");
   });
+});
 
 let position = { x: 0, y: 0 };
 const guiWindows = document.getElementsByClassName("movable-box");
@@ -35,10 +35,10 @@ interact(".movable-box").draggable({
       position.x = event.rect.left;
       position.y = event.rect.top - 50; // height of header
       for (let item of guiWindows) {
-		// console.log(item);
-		if ( item.classList.contains("active-window") ) {
-			item.classList.remove("active-window");
-		}
+        // console.log(item);
+        if (item.classList.contains("active-window")) {
+          item.classList.remove("active-window");
+        }
       }
       event.target.classList.add("active-window");
     },
