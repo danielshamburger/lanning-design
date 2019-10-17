5ced82f139aff8a18bee52a4486b1e0dfc8462da
Daniel Shamburger <daniel@wearetbx.com>
2019-10-17 14:34:36 -0400

    some styling and add design files

--- main.js
+++ main.js
@@ -1,36 +1,47 @@
-let position = { x: 0, y: 0 }
+let position = { x: 0, y: 0 };
 
-const guiWindows = document.getElementsByClassName('movable-box')
+const guiWindows = document.getElementsByClassName("movable-box");
 
-interact('.movable-box')
-    .draggable({
-      listeners: {
-        start (event) {
-          position.x = event.rect.left
-          position.y = event.rect.top - 73 // height of header
-          // console.log(document.getElementsByClassName('window'))
-          for (let item of guiWindows) {
-            // console.log(item);
-            item.classList.remove('active-window')
-          } 
-          // document.getElementsByClassName('window').item()
-          event.target.classList.add('active-window')
-        },
-        move (event) {
-          position.x += event.dx
-          position.y += event.dy
+/**
+ * General "window behavior"
+ */
 
-          event.target.style.left =
-            `${position.x}px`
+const closeButtons = document.getElementsByClassName("close-window");
 
-            event.target.style.top =
-              `${position.y}px`
-        },
-        },
-      allowFrom: '.drag-handle',
-      modifiers: [
-          interact.modifiers.restrictRect({
-            restriction: 'parent'
-          })
-      ]
+function closeWindow() {
+	document.getElementById(this.dataset.close).style.display = 'none';
+}
+
+Array.from(closeButtons).forEach(function(element) {
+	element.addEventListener('click', closeWindow);
   });
+
+interact(".movable-box").draggable({
+  listeners: {
+    start(event) {
+      position.x = event.rect.left;
+      position.y = event.rect.top - 50; // height of header
+      for (let item of guiWindows) {
+		// console.log(item);
+		if ( item.classList.contains("active-window") ) {
+			item.classList.remove("active-window");
+		}
+      }
+      event.target.classList.add("active-window");
+    },
+    move(event) {
+      position.x += event.dx;
+      position.y += event.dy;
+
+      event.target.style.left = `${position.x}px`;
+
+      event.target.style.top = `${position.y}px`;
+    }
+  },
+  allowFrom: ".drag-handle",
+  modifiers: [
+    interact.modifiers.restrictRect({
+      restriction: "parent"
+    })
+  ]
+});
